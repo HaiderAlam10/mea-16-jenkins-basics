c58f040b15b1bb707165b219d7e65eda2f982db1
Haider Alam <alamhaider1@gmail.com>
2023-12-05 09:47:20 +0000

    message

--- run copy.sh
+++ run copy.sh
@@ -1,3 +1,8 @@
-echo "I am running away from this course"
-echo "Yippee !!"
-echo "added webhook"
+echo "I am running away from this course" > 1.txt
+echo "Yippee !!" > 2.txt
+echo "added webhook" > 3.txt
+echo "another line" > 4.txt
+echo "another 5 line" > 5.txt
+
+zip archive.zip *.txt
+
